7f723fe24a3376686b757045422aaf9bf7bbed26
Vlad Gheorghita <vlad.gheorghita@arnia.ro>
2018-04-19 10:21:25 +0300

    index.ts exporting

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { FormsModule } from '@angular/forms';
 import { HelloService } from './hello-world/hello.service';
-import { AppLayoutComponent } from './layout/layout.component';
-import { AppHeaderComponent } from './layout/header/header.component';
-import { AppFooterComponent } from './layout/footer/footer.component';
 
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-
+import { AppLayoutComponent, AppFooterComponent, AppHeaderComponent } from './layout';
+import { AboutComponent, ContactComponent } from './static';
 
 const routes: Routes = [
   {
